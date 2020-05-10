7b6f54d43cbcff880e5035eab460d9bbf09c06ea
zara ahmadi <zatokey@gmail.com>
2020-05-10 19:26:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort 
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
